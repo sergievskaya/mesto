a9e913c21e8a836378c584f6d42d249eee3d0719
Ekaterina Sergievskaya <sergievskaya_ea@mail.ru>
2022-05-23 00:10:27 +0700

    feat: добавить js

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const closePopupButton = document.querySelector('.popup__close');
+
+const titleElement = document.querySelector('.profile__title');
+const nameInput = document.querySelector('.popup__input-name');
+const jobInput = document.querySelector('.popup__input-job');
+const subtitleElement = document.querySelector('.profile__subtitle');
+const formElement = document.querySelector('.popup__form');
+
+
+function openPopup(popupElement) {
+    popupElement.classList.add('popup__opened')
+}
+
+function closePopup(popupElement) {
+    popupElement.classList.remove('popup__opened')
+}
+
+editButton.addEventListener('click', function() {
+    openPopup(popup)
+    nameInput.value = titleElement.textContent
+    jobInput.value = subtitleElement.textContent
+})
+
+closePopupButton.addEventListener('click', function() {
+    closePopup(popup)
+})
+
+formElement.addEventListener('submit', function(event){
+    event.preventDefault()
+    titleElement.textContent = nameInput.value
+    subtitleElement.textContent = jobInput.value
+    closePopup(popup)
+})
