f969a91aeaf1b628e74f5ebe480853d03937a610
Ekaterina Sergievskaya <sergievskaya_ea@mail.ru>
2022-07-23 22:31:53 +0700

    fix: исправить ошибки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,74 +1,39 @@
-const popup = document.querySelectorAll('.popup');
+const popups = document.querySelectorAll('.popup');
 const popupProfile = document.querySelector('.popup_type_profile');
-const editButton = document.querySelector('.profile__edit-button');
-const closePopupButton = document.querySelectorAll('.popup__close');
+const editProfileButton = document.querySelector('.profile__edit-button');
 const titleElement = document.querySelector('.profile__title');
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
 const subtitleElement = document.querySelector('.profile__subtitle');
 const formElementProfile = document.querySelector('#form-profile');
-const addButton = document.querySelector('.profile__add-button');
+const addCardButton = document.querySelector('.profile__add-button');
 const popupCard = document.querySelector('.popup_type_card');
 const formElementCard = document.querySelector('#form-card');
-const initialCards = [
-    {
-      name: 'Норвегия',
-      link: 'https://images.unsplash.com/photo-1656802478225-2fc5f2db08fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDU4fDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
-    },
-    {
-      name: 'Дубай',
-      link: 'https://images.unsplash.com/photo-1656917051695-8a6d8df727f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
-    },
-    {
-      name: 'Черепаха',
-      link: 'https://images.unsplash.com/photo-1656004953298-9de99b01941c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=381&q=80'
-    },
-    {
-      name: 'Шотландия',
-      link: 'https://images.unsplash.com/photo-1658143290270-5e58c837d9d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-    },
-    {
-      name: 'Птица',
-      link: 'https://images.unsplash.com/photo-1507513102283-2c608c88fd0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-    },
-    {
-      name: 'Альпы',
-      link: 'https://images.unsplash.com/photo-1656925368663-f7e9cfb9c466?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-    }
-  ];
 const cardsList = document.querySelector('.elements__list');
 const titleInput = document.querySelector('.popup__input_type_title');
 const linkInput = document.querySelector('.popup__input_type_link');
 const popupOpenImage = document.querySelector('.popup_type_open-image');
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.elements__item');
 
 function openPopup(popupElement) {
     popupElement.classList.add('popup_opened');
 };
 
-// три функия для закрытия любого попапа
 function closePopup(popupElement) {
     popupElement.classList.remove('popup_opened');
 };
 
-function closePopupClick(evt) {
-    closePopup(evt.target.closest('.popup'));
-};
-
-closePopupButton.forEach(function(button) {
-    button.addEventListener('click', closePopupClick);
-});
-
-// закрыть попап при клике мимо
-popup.forEach(function(popupElement) {
+// закрыть попап при клике
+popups.forEach(function(popupElement) {
     popupElement.addEventListener('click', function(evt) {
-        if (evt.target.classList.contains('popup')) {
-         closePopup(popupElement);
+        if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close')) {
+            closePopup(popupElement);
         }
     })
 });
 
 // открытие попапа профиля
-editButton.addEventListener('click', function() {
+editProfileButton.addEventListener('click', function() {
     openPopup(popupProfile);
     nameInput.value = titleElement.textContent;
     jobInput.value = subtitleElement.textContent;
@@ -83,7 +48,7 @@ formElementProfile.addEventListener('submit', function(evt){
 });
 
 //открытие попапа добавления карточки
-addButton.addEventListener('click', function() {
+addCardButton.addEventListener('click', function() {
     openPopup(popupCard);
 });
 
@@ -112,16 +77,16 @@ function openImage(evt){
 
 // создание карточки
 function createCard(title, link) {
-    const cardTemplate = document.querySelector('#card-template').content;
-    const card = cardTemplate.querySelector('.elements__item').cloneNode(true);
+    const card = cardTemplate.cloneNode(true);
 
+    const cardImage = card.querySelector('.elements__image');
+    cardImage.setAttribute('src', link);
+    cardImage.setAttribute('alt', title);
     card.querySelector('.elements__text').textContent = title;
-    card.querySelector('.elements__image').setAttribute('src', link);
-    card.querySelector('.elements__image').setAttribute('alt', title);
 
     card.querySelector('.elements__delete-button').addEventListener('click', deleteCard);
     card.querySelector('.elements__like-button').addEventListener('click', likeCard);
-    card.querySelector('.elements__image').addEventListener('click', openImage);
+    cardImage.addEventListener('click', openImage);
 
     return card;
 };
